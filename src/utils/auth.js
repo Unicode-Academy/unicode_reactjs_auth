8eac61ff4371dbac9db0a0d56cfcff3e39dbe502
Hoàng An F8 <hoangan@fullstack.edu.vn>
2024-08-03 18:40:07 +0700

    Auth - 05

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -88,3 +88,54 @@ export const requestRefreshToken = async () => {
   }
   return false;
 };
+
+/*
+Request 1 ==> Success
+Request 2 ==> Expired ==> Refresh Token ==> Request 2
+Request 3 ==> Expired ==> Refresh Token ==> Request 3
+Request 4 ==> Expired ==> Refresh Token ==> Request 4
+
+Vấn đề: Refresh Token 3 lần
+
+Giải pháp
+
+Request 1 ==> Success
+Request 2 ==> Expired ==> Refresh Token ==> Request 2 
+                                                      Request 3
+                                                      Request 4
+*/
+
+// let token = `My Token`;
+// let isExpired = false;
+// let refreshTokenPromise = null;
+// const refreshToken = () => {
+//   return new Promise((resolve) => {
+//     setTimeout(() => {
+//       const newToken = `New Token: ${Math.random()}`;
+//       resolve(newToken);
+//     }, 2000);
+//   });
+// };
+// const callApi = (url) => {
+//   setTimeout(async () => {
+//     if (url === `/users`) {
+//       isExpired = true;
+//     }
+//     if (isExpired) {
+//       if (!refreshTokenPromise) {
+//         refreshTokenPromise = refreshToken();
+//       }
+//       const newToken = await refreshTokenPromise;
+//       token = newToken;
+//       isExpired = false;
+//     }
+
+//     console.log(`Call API: ${url}`, `Token: ${token}`);
+//   }, 1000);
+// };
+
+// callApi("/profile");
+// callApi("/users");
+// callApi("/courses");
+// callApi("/posts");
+// callApi("/products");
